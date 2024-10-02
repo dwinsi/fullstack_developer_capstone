c85a6401e240e4e20c71373983315b827a8a64d0
Ashwin <winsimash@gmail.com>
2024-10-03 01:12:55 +0530

    trivia

--- server/database/app.js
+++ server/database/app.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+/*jshint esversion: 8 */
 
 
 const express = require('express');
@@ -8,7 +9,7 @@ const  cors = require('cors');
 const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -23,10 +24,10 @@ const Dealerships = require('./dealership');
 
 try {
   Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+    Reviews.insertMany(reviews_data.reviews);
   });
   Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
   
 } catch (error) {
